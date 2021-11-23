63616b9c63837935d909cb3501effa3f9006cf38
stanblas83 <92794112+stanblas83@users.noreply.github.com>
2021-11-23 14:06:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from time import sleep
+from AllumeCamera import AllumeCamera
+from retourALaLigne import retourALaLigne
+from ajouter import ajouter
+from EteintCamera import EteintCamera
+import picamera
 
+#Initialisation de la caméra et création du fichier de donées 
+
+camera = picamera.Picamera()
 f = open('donnees.txt','w')
 f.write('TempÃ©rature;Altitude;Longitude;Pression,HumiditÃ©...')
 f.close()
+
 AllumeCamera()
 
 while():
 	retourALaLigne()
-	sleep(10)
+	sleep(20)
 	ajouter(TempÃ©rature)
 	ajouter(Altitude)
 	ajouter(Pression)
 	...
 	
 EteindreCamera()
+MP4Box -add my_video.h264 video.mp4
