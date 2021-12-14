e1525bcf2ed61c91991c542aa9b61294c5b9fef2
stanblas83 <92794112+stanblas83@users.noreply.github.com>
2021-12-14 13:46:08 +0100

    Update testMPL3115A2.py

--- testMPL3115A2.py
+++ testMPL3115A2.py
@@ -1,29 +1,26 @@
 from time import sleep
-from AllumeCamera import AllumeCamera
-from retourALaLigne import retourALaLigne
 from ajouter import ajouter
-from EteintCamera import EteintCamera
-import picamera
-import board
-import adafruit_mpl3115a2
+from retourALaLigne import retourALaLigne
 import capteurMPL3115A2
+import picamera
+from AllumeCamera import AllumeCamera
+from EteintCamera import EteintCamera
 
+sensor=capteurMPL3115A2.Initialisation()
 camera=picamera.PiCamera()
-AllumeCamera(camera)
-sensorMPL3115A2 = InitialisationMPL3115A2()
-
 f = open ( 'donnees.txt' , 'w' )
-f.write  ( 'Temperature,Altitude,Pression,' )
+f.write  ( 'Pression,Altitude,Temperature,' )
 f.close()
 n=0
-h=0
-while n<=5 : 
+
+AllumeCamera(camera)
+
+while n<5 : 
 	retourALaLigne()
-	sleep( 5 )
-	ajouter(capteurMPL3115A2.Temperature(sensorMPL3115A2))
-	ajouter(capteurMPL3115A2.Altitude(sensorMPL3115A2))
-  ajouter(capteurMPL3115A2.Pression(sensorMPL3115A2))
+	ajouter(capteurMPL3115A2.Pression(sensor))
+	ajouter(capteurMPL3115A2.Altitude(sensor))
+	ajouter(capteurMPL3115A2.Temperature(sensor))
+	sleep(2)
 	n=n+1
 
 EteintCamera(camera)
-MP4Box -add my_video.h264 video.mp4
