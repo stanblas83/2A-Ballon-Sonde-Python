9320d3602bd86df8521957a0200c39b939985abd
stanblas83 <92794112+stanblas83@users.noreply.github.com>
2021-12-14 13:44:39 +0100

    Update capteurMPL3115A2.py

--- capteurMPL3115A2.py
+++ capteurMPL3115A2.py
@@ -1,17 +1,17 @@
 import board
 import adafruit_mpl3115a2
 
-def InitailisationMPL3115A2():
-  i2c = boardI2C()
-  sensor=adafruit_mpl3115a2.MPL3115A2(i2c)
-  sensor.sealevel_pressure = 102250
-  return sensor
+def Initialisation():
+	i2c=board.I2C()
+	sensor=adafruit_mpl3115a2.MPL3115A2(i2c)
+	sensor.sealevel_pressure=102250
+	return sensor
 
 def Altitude(sensor):
-  return sensor.altitude
+	return str(sensor.altitude)
 
 def Pression(sensor):
-  return sensor.pressure
+	return str(sensor.pressure)
 
 def Temperature(sensor):
-  return sensor.temperature
+	return str(sensor.temperature)
