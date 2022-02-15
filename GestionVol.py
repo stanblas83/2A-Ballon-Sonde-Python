6e1024c4e4660e0c3f8d92d88af12dd9638d3fec
thomadam36 <92513123+thomadam36@users.noreply.github.com>
2022-02-15 14:48:23 +0100

    Update GestionVol.py

--- GestionVol.py
+++ GestionVol.py
@@ -1,23 +1,33 @@
 from AllumeCamera import AllumeCamera
-from Capteurs import Capteurs
+from ajouter import ajouter
+from retourALaLigne import retourALaLigne
 from EteintCamera import EteintCamera
-from EteintRaspberry import EteintRaspberry
+import capteurMPL3115A2
+import capteurDHT22
+import picamera
 from time import sleep
 
-def GestionVol () :
-  a1=0
-  a2=-1000
-  AllumeCamera()
-  a1=Capteurs()
-  sleep(60)
-  while (a1>5000 | a1>=a2)
-    a2=a1
-    a1=Capteurs()
-    sleep(10)
-  EteintCamera()
-  while (a1<a2)
-    a2=a1
-    a1=Capteurs()
-    sleep(10)
-  EteintRaspberry();
-  return
+sensorMPL=capteurMPL3115A2.Initialisation()
+sensorDHT=capteurDHT22.Initialisation()
+camera=picamera.PiCamera()
+fichier=open('données.txt','w')
+f.write('Pression,Altitude,TemperatureMPL,TemperatureDHT,Humidité')
+a2=-1000
+a1=capteurMPL3115A2.Altitude(sensorMPL)
+sleep(30)
+AllumeCamera(camera)
+
+while (a2<0)
+  retourALaLigne()
+  ajouter(capteurMPL3115A2.Pression(sensorMPL))
+	ajouter(a2)
+	ajouter(capteurMPL3115A2.Temperature(sensorMPL))
+  ajouter(capteurDHT22.Temperature(sensorDHT22))
+	ajouter(capteurDHT22.Humidite(sensorDHT22))
+  print(capteurMPL3115A2.Temperature(sensorMPL))
+  print(capteurDHT22.Temperature(sensorDHT22))
+  a2=a2+10
+  sleep(5)
+EteintCamera(camera)
+
+
